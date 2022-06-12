7efaf4b61067b4147e5710d1867ff8ef434e9a9f
Siddhant <santoshshrivastava39@gmail.com>
2022-06-12 17:37:12 +0530

    UC3 solution added

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -6,18 +6,35 @@ namespace SnakeAndLadderProblem
     {
         static void Main(string[] args)
         {
+         
             int startposition = 0;
-
-            Console.WriteLine("Please enter your name :");
-            string name = Console.ReadLine();
-
-            Console.WriteLine( name+ " your current position is :" + startposition);
+            Console.WriteLine("Current Position is " + startposition);
 
             Random random = new Random();
             int number = random.Next(1, 7);
+            Console.WriteLine("The number appeared on dice is " + number);
 
+            int option = random.Next(0, 3);
 
-            Console.WriteLine(" Now the number appeared is:" + number);
-        }
+            switch (option)
+            {
+                case 0:
+                    Console.WriteLine("No play");
+                    
+                    break;
+                case 1:
+                    Console.WriteLine("Yeah!Ladder");
+                    startposition = startposition + number;
+                    Console.WriteLine("Now your position is " + startposition);
+                    break;
+                case 2:
+                    Console.WriteLine("Oops!Snake");
+                   
+                    startposition = startposition - number;
+
+                    Console.WriteLine("Again come back to " + startposition);
+                    break;
+
+            }   }   
     }
 }
