a6a84de7c5d8d75be277c81955b5ef0ea40b57bb
Siddhant <santoshshrivastava39@gmail.com>
2022-06-12 17:04:16 +0530

    UC2 solution added

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -11,7 +11,13 @@ namespace SnakeAndLadderProblem
             Console.WriteLine("Please enter your name :");
             string name = Console.ReadLine();
 
-            Console.WriteLine( name+ " Your current position is :" + startposition);
+            Console.WriteLine( name+ " your current position is :" + startposition);
+
+            Random random = new Random();
+            int number = random.Next(1, 7);
+
+
+            Console.WriteLine(" Now the number appeared is:" + number);
         }
     }
 }
